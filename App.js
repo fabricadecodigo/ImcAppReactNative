9aada1c5d975d3be8313cb2f5e6edcd36810568d
Felipe <feliperfranco@gmail.com>
2019-09-03 09:06:10 -0300

    dividindo em componentes

--- App.js
+++ App.js
@@ -1,174 +1,51 @@
 import React, { Component } from "react";
 import {
-  StyleSheet,
-  Text,
+  StyleSheet,  
   View,
-  TextInput,
-  TouchableOpacity
 } from "react-native";
+
+import ImcResult from "./src/imc/ImcResult";
+import ImcForm from "./src/imc/ImcForm";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      peso: "",
-      altura: "",
-      showResult: false,
-      imc: "",
-      classificacao: "",
-      grauObesidade: ""
-    };
   }
+
+  state = {
+    showResult: false,
+    imc: "",
+    classificacao: "",
+    grauObesidade: ""
+  };  
 
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.form}>
-          <Text style={styles.label}>Altura</Text>
-          <TextInput
-            style={styles.input}
-            keyboardType={"numeric"}
-            placeholder="Altura (ex.: 1,70)"
-            onChangeText={altura => this.setState({ altura })}
-            value={this.state.altura}
-          />
-
-          <Text style={styles.label}>Peso</Text>
-          <TextInput
-            style={styles.input}
-            keyboardType={"numeric"}
-            placeholder="Peso (ex.: 75)"
-            onChangeText={peso => this.setState({ peso })}
-            value={this.state.peso}
-          />
-
-          <TouchableOpacity onPress={this.onSubmit} style={styles.button}>
-            <Text style={styles.buttonText}>Calcular</Text>
-          </TouchableOpacity>
-        </View>
+        <ImcForm showResult={this.showResult} />
+        
         {/* condicional para exibir ou não a parte abaixo */}
         {this.state.showResult && (
-          <View>
-            <Text style={styles.resultTitle}>IMC: {this.state.imc}</Text>
-            <Text style={styles.resultDescription}>
-              Classificação: {this.state.classificacao}
-            </Text>
-            <Text style={styles.resultDescription}>
-              Grau de obesidade: {this.state.grauObesidade}
-            </Text>
-          </View>
+          <ImcResult imc={this.state.imc} classificacao={this.state.classificacao} grauObesidade={this.state.grauObesidade} />
         )}
+        
       </View>
     );
-  }
+  }  
 
-  onSubmit = () => {
-    if (!this.state.peso || !this.state.altura) return;
-
-    let peso = 0;
-    if (this.state.peso) {
-      peso = parseFloat(this.state.peso);
-    }
-
-    let altura = 0;
-    if (this.state.altura) altura = parseFloat(this.state.altura);
-
-    const imc = peso / (altura * altura);
-    const classificacao = this.getClassificacao(imc);
-    const grauObesidade = this.getGrauObesidade(imc);
-
+  showResult = (result) => {
     this.setState({
       showResult: true,
-      imc: imc.toFixed(2),
-      classificacao: classificacao,
-      grauObesidade: grauObesidade
+      imc: result.imc,
+      classificacao: result.classificacao,
+      grauObesidade: result.grauObesidade
     });
-
-    // alert(
-    //   `Seu IMC é de: ${imc.toFixed(2)}. Classificação: ${classificacao}. Grau de obesidade: ${grauObesidade}`
-    // );
-  };
-
-  getClassificacao = imc => {
-    if (imc < 18.5) {
-      return "Magreza";
-    } else if (imc >= 18.5 && imc <= 24.9) {
-      return "Normal";
-    } else if (imc >= 25 && imc <= 29.9) {
-      return "Sobrepeso";
-    } else if (imc >= 30 && imc <= 39.9) {
-      return "Obesidade";
-    } else {
-      return "Obesidade grave";
-    }
-  };
-
-  getGrauObesidade = imc => {
-    if (imc < 18.5) {
-      return "0";
-    } else if (imc >= 18.5 && imc <= 24.9) {
-      return "0";
-    } else if (imc >= 25 && imc <= 29.9) {
-      return "I";
-    } else if (imc >= 30 && imc <= 39.9) {
-      return "II";
-    } else {
-      return "III";
-    }
-  };
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    //backgroundColor: "#CCC",
     padding: 10
-  },
-  form: {
-    marginTop: 30
-  },
-  input: {
-    paddingVertical: 6,
-    paddingHorizontal: 12,
-    borderWidth: 1,
-    borderColor: "#ced4da",
-    borderRadius: 4,
-    fontSize: 16,
-    fontWeight: "400",
-    backgroundColor: "#ffffff",
-    marginBottom: 16
-  },
-  label: {
-    fontSize: 16,
-    fontWeight: "400",
-    color: "#212529"
-  },
-  button: {
-    color: "#fff",
-    backgroundColor: "#007bff",
-    borderColor: "#007bff",
-    fontSize: 16,
-    fontWeight: "400",
-    alignItems: "center",
-    borderWidth: 1,
-    paddingVertical: 12,
-    paddingHorizontal: 12,
-    borderRadius: 4
-  },
-  buttonText: {
-    color: "#FFF"
-  },
-  resultTitle: {
-    fontSize: 20,
-    fontWeight: "400",
-    textAlign: "center",
-    marginTop: 20,
-    marginBottom: 10
-  },
-  resultDescription: {
-    fontSize: 16,
-    fontWeight: "400",
-    textAlign: "center",
-    color: "#333333"
   }
 });
---
+++ src/imc/ImcResult.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default class ImcResult extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View>
+        <Text style={styles.title}>IMC: {this.props.imc}</Text>
+        <Text style={styles.description}>Classificação: {this.props.classificacao}</Text>
+        <Text style={styles.description}>Grau de obesidade: {this.props.grauObesidade}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 20,
+    fontWeight: '400',
+    textAlign: 'center',
+    marginTop: 20,
+    marginBottom: 10
+  },
+  description: {
+    fontSize: 16,
+    fontWeight: '400',
+    textAlign: 'center',
+    color: '#333333'
+  }
+});
+
+
