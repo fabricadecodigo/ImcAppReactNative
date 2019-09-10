4a6cc4a83f423d1a1148740689713983075e6043
Felipe <feliperfranco@gmail.com>
2019-09-10 08:54:02 -0300

    versao inicial do app

--- App.js
+++ App.js
@@ -1,38 +1,178 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-});
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity
+} from 'react-native';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  state = {
+    peso: '',
+    altura: '',
+    showResult: false,
+    imc: '',
+    classificacao: '',
+    grauObesidade: ''
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <View style={styles.form}>
+          <Text style={styles.label}>Altura</Text>
+          <TextInput
+            style={styles.input}
+            keyboardType={'numeric'}
+            placeholder="Altura (ex.: 1.70)"
+            onChangeText={altura => this.setState({ altura })}
+            value={this.state.altura}
+          />
+
+          <Text style={styles.label}>Peso</Text>
+          <TextInput
+            style={styles.input}
+            keyboardType={'numeric'}
+            placeholder="Peso (ex.: 75)"
+            onChangeText={peso => this.setState({ peso })}
+            value={this.state.peso}
+          />
+
+          <TouchableOpacity onPress={this.onSubmit} style={styles.button}>
+            <Text style={styles.buttonText}>Calcular</Text>
+          </TouchableOpacity>
+        </View>
+        {this.state.showResult && (
+          <View>
+            <Text style={styles.resultTitle}>IMC: {this.state.imc}</Text>
+            <Text style={styles.resultDescription}>
+              Classificação: {this.state.classificacao}
+            </Text>
+            <Text style={styles.resultDescription}>
+              Grau de obesidade: {this.state.grauObesidade}
+            </Text>
+          </View>
+        )}
       </View>
     );
   }
+
+  onSubmit = () => {
+    if (!this.state.peso || !this.state.altura) return;
+
+    let peso = 0;
+    if (this.state.peso) {
+      peso = parseFloat(this.state.peso);
+    }
+
+    let altura = 0;
+    if (this.state.altura) altura = parseFloat(this.state.altura);
+
+    const imc = peso / (altura * altura);
+    const classificacao = this.getClassificacao(imc);
+    const grauObesidade = this.getGrauObesidade(imc);
+
+    this.setState({
+      showResult: true,
+      imc: imc.toFixed(2),
+      classificacao: classificacao,
+      grauObesidade: grauObesidade
+    });
+  };
+
+  getClassificacao = imc => {
+    if (imc < 18.5) {
+      return 'Magreza';
+    } else if (imc >= 18.5 && imc <= 24.9) {
+      return 'Normal';
+    } else if (imc >= 25 && imc <= 29.9) {
+      return 'Sobrepeso';
+    } else if (imc >= 30 && imc <= 39.9) {
+      return 'Obesidade';
+    } else {
+      return 'Obesidade grave';
+    }
+  };
+
+  getGrauObesidade = imc => {
+    if (imc < 18.5) {
+      return '0';
+    } else if (imc >= 18.5 && imc <= 24.9) {
+      return '0';
+    } else if (imc >= 25 && imc <= 29.9) {
+      return 'I';
+    } else if (imc >= 30 && imc <= 39.9) {
+      return 'II';
+    } else {
+      return 'III';
+    }
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    padding: 10
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
+    marginBottom: 5
   },
+  form: {
+    marginTop: 30
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: '400',
+    color: '#212529'
+  },
+  input: {
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderWidth: 1,
+    borderColor: '#ced4da',
+    borderRadius: 4,
+    fontSize: 16,
+    fontWeight: '400',
+    backgroundColor: '#ffffff',
+    marginBottom: 16
+  },
+  button: {
+    backgroundColor: '#007bff',
+    borderColor: '#007bff',
+    fontSize: 16,
+    fontWeight: '400',
+    alignItems: 'center',
+    borderWidth: 1,
+    paddingVertical: 12,
+    paddingHorizontal: 12,
+    borderRadius: 4
+  },
+  buttonText: {
+    color: '#FFF'
+  },
+  resultTitle: {
+    fontSize: 20,
+    fontWeight: '400',
+    textAlign: 'center',
+    marginTop: 20,
+    marginBottom: 10
+  },
+  resultDescription: {
+    fontSize: 16,
+    fontWeight: '400',
+    textAlign: 'center',
+    color: '#333333'
+  }
 });
